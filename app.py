030b55498319d00fb81597b47622dc1f007d82e7
nautanasgiseda <113368900+nautanasgiseda@users.noreply.github.com>
2023-05-15 09:32:24 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify
+from markupsafe import escape
+from flask import url_for
+from flask import request
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Response !!!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+# @app.route('/login')
+# def login():
+#     return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def helloo(name=None):
+    return render_template('saitas.html', name=name)
+
+# with app.test_request_context('/hello', method='POST'):
+#     # now you can do something with the request until the
+#     # end of the with block, such as basic assertions:
+#     assert request.path == '/hello'
+#     assert request.method == 'POST'
+
