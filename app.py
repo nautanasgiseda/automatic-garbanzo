0e3fc0bba4aca1a2ecc2f2e012fa85a0f1564f35
nautanasgiseda <113368900+nautanasgiseda@users.noreply.github.com>
2023-05-29 08:14:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def helloo(name=None):
     return render_template('saitas.html', name=name)
 
 # with app.test_request_context('/hello', method='POST'):
-#     # now you can do something with the request until the
+#     # now you can do something witth the request until the
 #     # end of the with block, such as basic assertions:
 #     assert request.path == '/hello'
 #     assert request.method == 'POST'
